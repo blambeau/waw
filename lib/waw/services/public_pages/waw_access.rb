fe9c521d6d32fffe330a3511f35ee981848aa122
BERNARD LAMBEAU <blambeau@gmail.com>
2010-01-12 12:50:50 +0100

    .wawaccess implementation classes started and basically tested.

--- lib/waw/services/public_pages/waw_access.rb
+++ lib/waw/services/public_pages/waw_access.rb
@@ -7,24 +7,67 @@ module Waw
         # Strategy :allow_all or :deny_all
         attr_accessor :strategy
         
+        # The parent wawaccess file
+        attr_accessor :parent
+        
         # Creates a default WawAccess file
         def initialize(read_file=nil)
           @strategy = :allow_all
           @serve = {}
-          if read_file
-            begin
-              contents = File.read(read_file)
-              WawAccess::DSL.new(self, read_file).instance_eval(contents)
-            rescue WawError => ex
-              raise ex
-            rescue Exception => ex
-              raise WawError, "Corrupted .wawaccess file #{read_file}: #{ex.message}", ex.backtrace
-            end
-          end
+          dsl_merge_file(read_file) if read_file
         end
+        
+        ################################################### About installation through DSL
         
         # Installs some pattern services
         def serve(patterns, &block)
+          block = Kernel.lambda{|url, pagefile, app| Hash.new } unless block
+          patterns.each do |pattern|
+            @serve[pattern] = block
+          end
+        end
+        
+        # Merges a DSL string inside this waw access
+        def dsl_merge(dsl_str, read_file=nil)
+          begin
+            WawAccess::DSL.new(self, read_file).instance_eval(dsl_str)
+          rescue WawError => ex
+            raise ex
+          rescue Exception => ex
+            raise WawError, "Corrupted .wawaccess file #{read_file}: #{ex.message}", ex.backtrace
+          end
+          self
+        end
+        
+        # Merge with a .wawaccess file
+        def dsl_merge_file(read_file)
+          raise WawError, "Missing .wawaccess file #{readfile}" unless File.file?(read_file)
+          dsl_merge(File.read(read_file), read_file)
+          self
+        end
+        
+        ################################################### About wawaccess itself
+        
+        # Finds the block used for serving a given file
+        def find_prepare_block(file, use_hierarchy = true)
+          ext = File.extname(file)
+          if @serve.has_key?(ext)
+            @serve[ext]
+          elsif use_hierarchy and parent
+            parent.find_prepare_block(file, true)
+          else
+            nil
+          end
+        end
+        
+        # Checks if this wawaccess file allows serving a given file
+        def may_serve?(file, use_hierarchy = true)
+          not(find_prepare_block(file).nil?)
+        end
+        
+        # Prepare the context for instantiating a given file
+        def prepare_context(url, file, app)
+          (block = find_prepare_block(file)) and block.call(url, file, app)
         end
         
       end # class WawAccess
--- test/unit/waw/services/public_pages/waw_access_test.rb
+++ test/unit/waw/services/public_pages/waw_access_test.rb
@@ -1,13 +1,63 @@
 require 'test/unit'
 require 'waw'
-
 module Waw
   module Services
     module PublicPages
       # Waw version of .htaccess files
       class WawAccessTest < Test::Unit::TestCase
         
-        def test_dsl
+        FIRST_WAW_ACCESS = <<-EOF
+          wawaccess do 
+            strategy :deny_all
+            serve '.wtpl' do |url, page, app|
+              {:message => 'serving .wtpl'}
+            end
+            serve '.html'
+          end
+        EOF
+        
+        SECOND_WAW_ACCESS = <<-EOF
+          wawaccess do 
+            serve '.html' do |url, page, app|
+              {:message => 'serving .html'}
+            end
+            serve '.brick' do |url, page, app|
+              {:message => 'serving .brick'}
+            end
+          end
+        EOF
+        
+        def test_dsl_on_first_waw_access
+          assert_nothing_raised do 
+            wawaccess = WawAccess.new.dsl_merge(FIRST_WAW_ACCESS)
+            assert_equal :deny_all, wawaccess.strategy
+            assert wawaccess.may_serve?('test.wtpl')
+            assert wawaccess.may_serve?('test.html')
+            assert !wawaccess.may_serve?('test.brick')
+            assert_equal 'serving .wtpl', wawaccess.prepare_context('/test', 'test.wtpl', self)[:message]
+            assert_equal({}, wawaccess.prepare_context('/test', 'test.html', self))
+          end
+        end
+        
+        def test_dsl_on_second_waw_access
+          assert_nothing_raised do 
+            wawaccess = WawAccess.new.dsl_merge(SECOND_WAW_ACCESS)
+            assert_equal :allow_all, wawaccess.strategy
+            assert !wawaccess.may_serve?('test.wtpl')
+            assert wawaccess.may_serve?('test.html')
+            assert wawaccess.may_serve?('test.brick')
+            assert_equal 'serving .html', wawaccess.prepare_context('/test', 'test.html', self)[:message]
+            assert_equal 'serving .brick', wawaccess.prepare_context('/test', 'test.brick', self)[:message]
+          end
+        end
+        
+        def test_wawaccess_hierarchy
+          first = WawAccess.new.dsl_merge(FIRST_WAW_ACCESS)
+          second = WawAccess.new.dsl_merge(SECOND_WAW_ACCESS)
+          assert !second.may_serve?('test.wtpl')
+          second.parent = first
+          assert_equal true, second.may_serve?('test.wtpl', true)
+          assert_equal 'serving .wtpl', second.prepare_context('/test', 'test.wtpl', self)[:message]
         end
         
       end
