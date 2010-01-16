77b007aeb2ceffacbda512945fb96ce3175e04ae
Bernard Lambeau <blambeau@gmail.com>
2010-01-16 18:49:43 +0100

    Adds a spec test for the missing validator.

--- lib/waw/validation/signature.rb
+++ lib/waw/validation/signature.rb
@@ -36,18 +36,33 @@ module Waw
           @validator = validator
           @onfailure = onfailure
         end
-      
-        # Validates argument values given through a hash
-        def validate(hash)
-          ok = @validator.validate(*@args.collect{|arg| hash[arg]})
-          ok ? nil : @onfailure
+        
+        # Collects the interesting parameters on a hash
+        def collect_on_hash(hash)
+          @args.collect{|arg| hash[arg]}
         end
       
-        # Converts and validate values given trough a hash
+        # Validates argument values given through a hash. Returns nil
+        # if everything is ok, the on_failure installed parameters otherwise.
+        def validate(hash)
+          @validator.validate(*collect_on_hash(hash)) ? nil : @onfailure
+        end
+      
+        # Validates and convert a hash. Values of the hash are replaced
+        # by their convertions if the validation passes. In this case, a
+        # nil value is returned. Otherwise, the on_failure is returned 
+        # and the hash is not changed.
         def convert_and_validate(hash)
-          ok, values = @validator.convert_and_validate(*@args.collect{|arg| hash[arg]})
+          ok, values = @validator.convert_and_validate(*collect_on_hash(hash))
           @args.each_with_index {|arg, i| hash[arg] = values[i]} if ok
           ok ? nil : @onfailure
+        end
+        
+        # Applies the validation and conversion rule on a hash. Returns the
+        # first converted (or non converted if validation fails) value.
+        def first_converted(hash)
+          ok, values = @validator.convert_and_validate(*collect_on_hash(hash))
+          values[0]
         end
       
       end # class Validation
@@ -88,6 +103,13 @@ module Waw
         converted, failures = hash.dup, []
         failures = @rules.collect{|rule| rule.convert_and_validate(converted)}.compact
         failures.empty? ? [true, converted] : [false, failures]
+      end
+      
+      # Calls first_converted(hash) on the first rule. Returns nil if no rule
+      # has been installed. The hash is never modified by this method.
+      def first_converted(hash = {})
+        return nil if @rules.nil? or @rules.empty?
+        @rules[0].first_converted(hash)
       end
       
       # Checks if the signature allows passing with some values 
