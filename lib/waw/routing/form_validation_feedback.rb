f1ea773c667fbf06c9c5cd8dbe898aacb7584a7d
Bernard Lambeau <blambeau@gmail.com>
2010-01-22 14:24:40 +0100

    Adds the ability to scroll on top of the window, form or form feedback when form validation fails.

--- lib/waw/routing/form_validation_feedback.rb
+++ lib/waw/routing/form_validation_feedback.rb
@@ -3,6 +3,10 @@ module Waw
     # FormValidationFeedback routing
     class FormValidationFeedback < RoutingRule
 
+      def initialize(opts = {})
+        @options = opts
+      end
+
       def generate_js_code(result, align=0)
         buffer = ""
 				buffer << " "*align + "str = '';\n"
@@ -11,8 +15,19 @@ module Waw
 				buffer << " "*(align+2)	+ "str += '<li>' + messages[data[1][k]] + '</li>';\n"
 				buffer << " "*align + "}\n"
 				buffer << " "*align + "str += '</ul>';\n"
-				buffer << " "*align + "$(form + ' .feedback').show();"
-				buffer << " "*align + "$(form + ' .feedback').html(str);"
+				buffer << " "*align + "$(form + ' .feedback').show();\n"
+				buffer << " "*align + "$(form + ' .feedback').html(str);\n"
+				if @options[:scroll]
+  				where = case @options[:scroll]
+    			  when :top
+  			      '0'
+  			    when :form
+  			      "$(form).offset().top"
+  			    when :feedback
+  			      "$(form + ' .feedback').offset().top"
+  			  end
+          buffer << "$('html, body').animate( { scrollTop: #{where} }, 'slow' );\n"
+  			end
         buffer
       end
       
