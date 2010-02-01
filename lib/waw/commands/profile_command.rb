804ed3684786d0235afd59929f49f7f9262241c2
Bernard Lambeau <blambeau@gmail.com>
2010-02-01 19:51:49 +0100

    waw-profile shows basic statistics now...

--- lib/waw/commands/profile_command.rb
+++ lib/waw/commands/profile_command.rb
@@ -29,7 +29,7 @@ module Waw
           puts "Hohoho, I've found a lost internal link #{location}"
           return
         else
-          puts "Visiting #{location}"
+          puts "Visiting #{location}" if verbose
           browser.all_internal_links.each do |link|
             visit(browser, link[:href], visited)
           end
@@ -39,12 +39,26 @@ module Waw
       # Runs the sub-class defined command
       def __run(requester_file, arguments)
         require 'waw/wspec/browser'
-        thread = ::Waw::Commands::StartCommand.new.waw_start(requester_file)
+        
+        # Start the server first
+        thread = ::Waw::Commands::StartCommand.new.waw_start(requester_file, verbose)
         visited, browser, location = {}, ::Waw::WSpec::Browser.new, Waw.config.web_base
+        
+        # Launch the visit
+        t1 = Time.now
         visit(browser, location, visited)
+        t2 = Time.now
+        
+        # Stop the server now
         thread.exit
+        
+        # Show statistics
+        req_by_sec = visited.size.to_f/(t2-t1)
+        avg_by_sec = (t2-t1)/visited.size
+        
+        puts "#{visited.size} pages visited in #{t2-t1} seconds (#{req_by_sec} req./sec., #{avg_by_sec} sec./req.)"
       rescue Interrupt => ex
-        info "waw-profile stopping now... ciao!"
+        info "waw-profile stopping now... ciao!" if verbose
       end
 
     end # module ProfileCommand
