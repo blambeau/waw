e9660ca7500a11f47c1724ad34dbb5983a44fee4
Bernard Lambeau <blambeau@gmail.com>
2010-03-19 18:41:33 +0100

    Makes change to the way error are handled.

--- lib/waw/controllers/error_handler.rb
+++ lib/waw/controllers/error_handler.rb
@@ -3,16 +3,40 @@ module Waw
   class ErrorHandler < ::Waw::Controller
     
     # Creates an error handler rack application
-    def initialize(app, *handlers)
+    def initialize(app)
       @app = app
-      @handlers = handlers
+      @validators, @handlers = [], []
     end
     
-    # Adds an error handler as a block. 
-    def add_handler(handler, &block)
-      raise ArgumentError, "Unable to install both handler" if (handler && block)
-      raise ArgumentError, "Missing block as error handler" unless (handler || block)
-      @handlers << (handler || block)
+    # Handles arguments of push and unshift
+    def handle_args(args, &block)
+      validator, handler = args
+      handler = block if handler.nil?
+      validator, handler = nil, validator if handler.nil?
+      [validator, handler]
+    end
+    
+    # Adds an error handler. The method accepts the following 
+    # argument variants:
+    #
+    #   add_error_handler{|k,ex| ... }
+    #   add_error_handler(SomeValidator){|k,ex| ... }
+    #   add_error_handler(SomeHandler)
+    #   add_error_handler(SomeValidator, SomeHandler)
+    #
+    def add_error_handler(*args, &block)
+      validator, handler = handle_args(args, &block)
+      @validators << validator
+      @handlers << handler
+    end
+    alias :push :add_error_handler 
+    
+    # Same as add_error_handler but adds it at the begining of the 
+    # chain
+    def unshift(*args, &block)
+      validator, handler = handle_args(args, &block)
+      @validators.unshift(validator)
+      @handlers.unshift(handler)
     end
     
     # Checks if a result seems a valid rack application result
@@ -25,9 +49,11 @@ module Waw
       @app.call(env)
     rescue Exception => ex
       kernel = find_kernel_context
-      @handlers.each do |handler|
-        result = handler.call(kernel, ex)
-        return result if seems_valid_result?(result)
+      @validators.each_with_index do |validator, i|
+        if (validator.nil? or (validator===ex))
+          result = @handlers[i].call(kernel, ex)
+          return result if seems_valid_result?(result)
+        end
       end
       raise ex
     end
